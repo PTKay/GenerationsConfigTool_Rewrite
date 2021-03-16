acea774d0b4949f0ee97d755cc1dab14c360fcee
PTKickass <jp_moura99@hotmail.com>
2021-03-16 01:40:49 +0000

    Implemented Nvidia Optimus support

--- GensConfigTool/App.xaml.cs
+++ GensConfigTool/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using ConfigurationTool.Handlers;
+using System;
+using System.Windows;
 
 namespace ConfigurationTool
 {
@@ -7,5 +9,15 @@ namespace ConfigurationTool
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                RegistryHandler.FixRegistry(int.Parse(args[1]));
+                Application.Current.Shutdown();
+            }
+            NvidiaHandler.InitializeDedicatedGraphics();
+        }
     }
 }
---
+++ GensConfigTool/Handlers/NvidiaHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConfigurationTool.Handlers
+{
+    class NvidiaHandler
+    {
+        [System.Runtime.InteropServices.DllImport("nvapi64.dll", EntryPoint = "fake")]
+        static extern int LoadNvApi64();
+
+        [System.Runtime.InteropServices.DllImport("nvapi.dll", EntryPoint = "fake")]
+        static extern int LoadNvApi32();
+
+        public static void InitializeDedicatedGraphics()
+        {
+            try
+            {
+                if (Environment.Is64BitProcess)
+                    LoadNvApi64();
+                else
+                    LoadNvApi32();
+            }
+            catch { } // will always fail since 'fake' entry point doesn't exists
+        }
+    }
+}
